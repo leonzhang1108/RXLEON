75f9fdbb4a01777bc376e718aaeca37a491d35a7
leonzhang <human1108@163.com>
2018-02-12 22:09:22 +0800

    every

---
+++ operators/every.js
@@ -0,0 +1,29 @@
+import Rx from 'rxjs'
+import { bindContext } from './util.js'
+
+const every = context => f => Rx.Observable.create(observer => {
+  let y = false
+
+  const next = x => {
+    y = f(x)
+    if(y) return
+    observer.next(y)
+    observer.complete()
+    subscription.unsubscribe()
+  }
+
+  const error = e => {
+    observer.error(e)
+  }
+
+  const complete = () => {
+    y && observer.next(y)
+    observer.complete()
+  }
+
+  const subscription = context.subscribe({ next, error, complete })
+
+  return subscription
+})
+
+module.exports = bindContext(every)
---
+++ test/every.js
@@ -0,0 +1,34 @@
+import assert from 'assert'
+import Rx from 'rxjs'
+Rx.Observable.fromArray = require('@observables/fromArray')
+Rx.Observable.prototype.every = require('@operators/every')
+
+describe('every', () => {
+  it('every true test', done => {
+    const list = [2,4,6,8,10]
+
+    Rx.Observable.fromArray(list)
+      .every(val => val % 2 === 0)
+      .subscribe({
+        next: x => {
+          assert.strictEqual(x, true)
+        },
+        error: () => done('error should not be called'),
+        complete: done
+      })
+  })
+
+  it('every false test', done => {
+    const list = [1,2,3,4,5]
+
+    Rx.Observable.fromArray(list)
+      .every(val => val % 2 === 0)
+      .subscribe({
+        next: x => {
+          assert.strictEqual(x, false)
+        },
+        error: () => done('error should not be called'),
+        complete: done
+      })
+  })
+})
