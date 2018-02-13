1193c73ff6a6a67d6ea58dc4c284f886137ab72c
leonzhang <leonzhang1108@163.com>
2018-02-14 03:20:40 +0800

    reduce

---
+++ operators/reduce.js
@@ -0,0 +1,23 @@
+import Rx from 'rxjs'
+import { bindContext } from './util.js'
+
+const reduce = context => (f, initVal) => Rx.Observable.create(observer => {
+  let store = initVal || 0
+
+  const next = x => {
+    store = f(store, x)
+  }
+
+  const error = e => {
+    observer.error(e)
+  }
+
+  const complete = () => {
+    observer.next(store)
+    observer.complete()
+  }
+
+  return context.subscribe({ next, error, complete })
+})
+
+module.exports = bindContext(reduce)
---
+++ test/reduce.js
@@ -0,0 +1,18 @@
+import assert from 'assert'
+import Rx from 'rxjs'
+Rx.Observable.of = require('@observables/of')
+Rx.Observable.prototype.reduce = require('@operators/reduce')
+
+describe('reduce', () => {
+  it('reduce test', done => {
+    Rx.Observable.of(1, 2, 3, 4)
+      .reduce((acc, val) => acc + val, 0)
+      .subscribe({
+        next: x => {
+          assert.strictEqual(x, 10)
+        },
+        error: () => done('error should not be called'),
+        complete: done
+      })
+  })
+})
